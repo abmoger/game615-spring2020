44fc47083e98d93256def6b7fba97eca47eed18b
abmoger <57551169+abmoger@users.noreply.github.com>
2020-01-28 20:59:55 -0500

    Added Breakout to repository

---
+++ exercises/exercise1/Breakout/Assets/BallMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public KeyCode launchButton;
+    public float ballVelocity = 75f;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+      if (Input.GetKey(launchButton))
+        {
+            transform.parent = null;
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, ballVelocity, 0));
+        }  
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "BottomWall")
+        {
+#pragma warning disable CS0618 // Type or member is obsolete
+            Application.LoadLevel(Application.loadedLevel);
+#pragma warning restore CS0618 // Type or member is obsolete
+        }
+    }
+}
---
+++ exercises/exercise1/Breakout/Assets/PaddleMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMovement : MonoBehaviour
+{
+    public float paddleSpeed = 0.1f;
+    public float paddleX;
+    public float paddleY;
+    public KeyCode leftKey;
+    public KeyCode rightKey;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        if (Input.GetKey(leftKey) && transform.position.x > -7.25f)
+        {
+            transform.position = new Vector3(transform.position.x - paddleSpeed, transform.position.y, 0);
+        }
+        if (Input.GetKey(rightKey) && transform.position.x < 7.25f)
+        {
+            transform.position = new Vector3(transform.position.x + paddleSpeed, transform.position.y, 0);
+        }
+    }
+}
